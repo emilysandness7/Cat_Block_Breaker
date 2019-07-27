e80b979a4539a9855699a4a45a79a3e6746440b8
EmilySandness7 <emilysandness@gmail.com>
2019-07-27 10:09:07 -0700

    First Commit

---
+++ Cat Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+
+    //loads the Game_Over scene when ball passes through collider
+    private void OnTriggerEnter2D(Collider2D collision) {
+        SceneManager.LoadScene("Game_Over");
+    }
+}
---
+++ Cat Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] int screenUnits = 16;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Gets the mouse's x position and does math so it is contained in the screen
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenUnits;
+
+        // Moves the paddle to the mouse's x location
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+	}
+}
