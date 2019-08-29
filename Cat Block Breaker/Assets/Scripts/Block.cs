59fcaebe8e855ddcc7046ba28bf8db717568eb25
EmilySandness7 <emilysandness@gmail.com>
2019-08-29 08:21:31 -0700

    Added Level GameObject. Level keeps track of how many breakable Blocks there are in a level. When Blocks are <= 0, Level calls SceneLoader to load next scene.

--- Cat Block Breaker/Assets/Scripts/Block.cs
+++ Cat Block Breaker/Assets/Scripts/Block.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    [SerializeField] AudioClip breakSound;
+
+    //cached references
+    Level level;
+
+    private void Start() {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) {
+        
+        DestroyBlock();
+    }
+
+    private void DestroyBlock() {
+        //create an AudioSource and play the break sound at Main Camera
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
